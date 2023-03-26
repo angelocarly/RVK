c42b25db1f4045bad99f0af95e13f29fb3bc3be0
Angelo Carly <angelo.carly@protonmail.com>
2023-03-26 13:27:06 +0200

    Add png image output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::path::Path;
+use std::io::BufWriter;
+
 fn main() {
-    println!("Hello, world!");
+
+    let path = Path::new(r"Output/out.png");
+    let file = File::create(path).unwrap();
+    let ref mut w = BufWriter::new(file);
+
+    let mut encoder = png::Encoder::new(w, 2, 1); // Width is 2 pixels and height is 1.
+    encoder.set_color( png::ColorType::Rgba );
+    encoder.set_depth( png::BitDepth::Eight );
+    let data = [ 0, 0, 255, 255, 0, 0, 0, 255 ];
+
+    let mut writer = encoder.write_header().unwrap();
+    writer.write_image_data( &data ).unwrap(); // Save
+
+    println!("Written image");
 }
